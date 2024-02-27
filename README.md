5d7b520d560060e99c0b6d5f225d8c9feab8aee8
overskilled <ouateedemloic@gmail.com>
2024-02-27 03:27:54 +0100

    Auth modifications

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 user pool ID: eu-north-1_wHDpebzEg
 client ID: 1rs30u8t1781sio4sjjnbbu3s1
+
+Access key: AKIAZEDKRI5MERQ37W6J
+secretAccessKey: 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
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "mydropbox52390bcf": {
+    "mydropboxbcd89db2": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
